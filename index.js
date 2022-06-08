b4594244210efa8ac8710f1a46710e8627e18537
artaly <villalonalycah@gmai.com>
2022-06-09 01:22:35 +0800

    idk what happened

---
+++ index.js
@@ -0,0 +1,7 @@
+function catLaugh() {
+    if (document.getElementById("catto").src == "https://i.imgur.com/Nqhs6Ue.png"){
+            document.getElementById("catto").src = "https://i.imgur.com/PsJW3PJ.png";
+    } else {
+            document.getElementById("catto").src = "https://i.imgur.com/Nqhs6Ue.png";
+    }
+}
