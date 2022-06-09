068c1d3954501d5ec815f83401742f239b8f46d4
artaly <villalonalycah@gmai.com>
2022-06-09 12:39:22 +0800

    Add sound code

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 function catLaugh() {
     if (document.getElementById("catto").src == "https://i.imgur.com/Nqhs6Ue.png"){
             document.getElementById("catto").src = "https://i.imgur.com/PsJW3PJ.png";
+            openMouthSound.play();
     } else {
             document.getElementById("catto").src = "https://i.imgur.com/Nqhs6Ue.png";
+            closeMouthSound.play();
     }
 }
 
 function instruc() {
         alert("Click the button continuously to make the cat laugh!");
 }
+
+const openMouthSound = new Audio("./sounds/sound_sound-open.mp3");
+const closeMouthSound = new Audio("./sounds/sound_sound-close.mp3");
