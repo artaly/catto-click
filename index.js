0cdefd82dc53709ecc4b7f84824548fcdbd6965e
artaly <villalonalycah@gmai.com>
2022-06-09 12:18:54 +0800

    final ig

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function catLaugh() {
             document.getElementById("catto").src = "https://i.imgur.com/Nqhs6Ue.png";
     }
 }
+
+function instruc() {
+        alert("Click the button continuously to make the cat laugh!");
+}
